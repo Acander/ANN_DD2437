85b77df08d0383ecf6017c869c08697cfc58b292
Acander <adrianz@kth.se>
2020-01-23 10:40:52 +0100

    prototype of single-perceptron nearly done

--- Lab1/SingleLayerPerceptron.py
+++ Lab1/SingleLayerPerceptron.py
@@ -6,16 +6,21 @@ orient the matrixes wrongly so that columns and rows are interchanged.
 Make a sketch on paper where you write down the sizes of all components 
 starting by the input and how the dimensionality propagatesto the weights to the output."""
 
+NUMBER_OF_DATA_POINTS = 100
 EPOCHS = 20
 LEARNING_RATE_n = 0.001
-NUMBER_OF_DIM = 2
-NUMBER_OF_COLUMNS = 5
 
+"""The weights are stored in matrix W with as many columns as the dimensionality of the 
+input patterns and with the number of rows matching the number of the outputs 
+(dimensionality of the output)."""
+
+NUMBER_OF_DIM = 2
+NUMBER_OF_COLUMNS = 1
 NUMBER_OF_ROWS = NUMBER_OF_DIM + 1
 
 def main():
     w_matrix = np.matrix[init_w_vectors_normal(0, 1)]
-    patterns, targets = init_training_data()
+    patterns, targets = init_training_data_normal()
 
     for e in range(EPOCHS):
         w_matrix = epoch_iteration(w_matrix, patterns, targets)
@@ -32,8 +37,26 @@ def init_w_vectors_normal(mean, variance):
 
 #TODO
 "Implement training data initialization using normal distribution"
-"""def init_training_data_normal"""
+def init_training_data_normal():
+    n = NUMBER_OF_DATA_POINTS
+    mA = [1.0, 0.5]
+    sigmaA = 0.5
+    mB = [-1.0, 0.0]
+    sigmaB = 0.5
+    classA = np.zeros(3)
+    classB = np.zeros(3)
 
+    classA[0] = np.random.normal(mA[0], sigmaA, n)
+    classA[1] = np.random.normal(mA[1], sigmaA, n)
+    classA[2] = np.zeros(NUMBER_OF_COLUMNS) + 1
+
+    classB[0] = np.random.normal(mB[0], sigmaB, n)
+    classB[1] = np.random.normal(mB[1], sigmaB, n)
+    classB[2] = np.zeros(NUMBER_OF_COLUMNS) + 1
+
+    return classA, classB
+
+"""
 def init_training_data():
     patterns = [[0, 1, 1],
                 [-1, 3, 1],
@@ -48,7 +71,7 @@ def init_training_data():
     targets = [1, -1, 1, -1, 1, -1, 1, -1]
 
     return patterns, targets
-
+"""
 def init_test_data():
     patterns = [[0, 4], [2, 4], [9, 9], [-1, -2], [-4, -5], [-2, -2]]
     patterns = make_matrix_compatible(patterns)
