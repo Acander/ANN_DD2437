a06b5dd8dfe1b89327feb02d0bd26c17fd43da2b
Acander <adrianz@kth.se>
2020-01-23 10:55:23 +0100

    prototype of single-perceptron nearly done

--- Lab1/SingleLayerPerceptron.py
+++ Lab1/SingleLayerPerceptron.py
@@ -20,16 +20,21 @@ NUMBER_OF_ROWS = NUMBER_OF_DIM + 1
 
 def main():
     w_matrix = np.matrix[init_w_vectors_normal(0, 1)]
-    patterns, targets = init_training_data_normal()
+    classA, classB = init_training_data_normal()
 
     for e in range(EPOCHS):
-        w_matrix = epoch_iteration(w_matrix, patterns, targets)
+        w_matrix = epoch_iteration(w_matrix, classA, classB)
 
 
-def epoch_iteration(w_matrix, patterns, targets):
-    delta_w = delta_rule(w_matrix, patterns, targets)
+def epoch_iteration(w_matrix, classA, classB):
+    delta_w = delta_rule(w_matrix, classA, classB)
     w_matrix = w_matrix - delta_w
     return w_matrix
+
+def delta_rule(weights, patterns, targets):
+    delta_w = -LEARNING_RATE_n*np.dot((np.dot(weights, patterns) - targets), np.transpose(patterns))
+    return delta_w
+
 
 def init_w_vectors_normal(mean, variance):
     w_matrix = np.random.normal(mean, variance, (NUMBER_OF_ROWS, NUMBER_OF_COLUMNS))
@@ -39,9 +44,9 @@ def init_w_vectors_normal(mean, variance):
 "Implement training data initialization using normal distribution"
 def init_training_data_normal():
     n = NUMBER_OF_DATA_POINTS
-    mA = [1.0, 0.5]
+    mA = [2, 1]
     sigmaA = 0.5
-    mB = [-1.0, 0.0]
+    mB = [-2, -1]
     sigmaB = 0.5
     classA = np.zeros(3)
     classB = np.zeros(3)
@@ -81,10 +86,6 @@ def make_matrix_compatible(matrix):
     matrix = np.matrix(matrix)
     matrix = matrix.transpose()
     return matrix
-
-def delta_rule(weights, patterns, targets):
-    delta_w = -LEARNING_RATE_n*np.dot((np.dot(weights, patterns) - targets), np.transpose(patterns))
-    return delta_w
 
 def plot_outcome():
     pb.
