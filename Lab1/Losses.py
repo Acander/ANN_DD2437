Fixed Backprop

Lab1/Losses.py
@@ -7,4 +7,4 @@ class MSE:
         return np.mean((y - label) ** 2)
 
     def derivative(self, y, label):
-        return 2 * np.mean(y - label)
+        return 2 * np.mean(y - label, axis=0).reshape((1, y.shape[1]))

Lab1/NeuralNetwork.py
@@ -12,11 +12,11 @@ class DenseLayer:
         self.cachePostActivation = 0
 
     def _initLayerWeights(self, sizeIn, sizeOut):
-        return np.random.normal(0, 0.5, (sizeIn, sizeOut))
+        return np.random.normal(0, 0.5, (sizeOut, sizeIn))
 
     def forward(self, x):
         self.cacheInput = x
-        self.cachePreActivation = np.matmul(x, self.weights)
+        self.cachePreActivation = np.matmul(self.weights, x)
         self.cachePostActivation = self.activation.forward(self.cachePreActivation)
         return self.cachePostActivation
 
@@ -29,33 +29,25 @@ class FeedForwardNet:
         self.loss = loss
         self.cache = []
 
-    # Ja
     def forwardPass(self, x):
+        x = x.T
         for l in self.layers:
             x = l.forward(x)
         return x
 
-
     def _backprop(self, labels):
         gradientProduct = self.loss.derivative(self.layers[-1].cachePostActivation, labels)
         for i, l in enumerate(reversed(self.layers)):
-            print("\n", i)
-            print("Gradient Product", gradientProduct.shape)
-            print("Activation grad:", l.activation.derivative(l.cachePreActivation).shape)
             gradientProduct *= l.activation.derivative(l.cachePreActivation)
-            print("Gradient Product", gradientProduct.shape)
-            print("Cache input", l.cacheInput.shape)
-            weightGrad = gradientProduct * l.cacheInput
-            print("Weight grad", weightGrad.shape)
+            weightGrad = np.matmul(gradientProduct, l.cacheInput.T)
 
-            l.weights -= weightGrad.reshape(l.weights.shape)
+            l.weights -= weightGrad * self.lr
+            gradientProduct = np.matmul(l.weights.T, gradientProduct)
+
 
     def fit(self, x, labels):
         y = self.forwardPass(x)
-        gradient = self._backprop(labels)
-        for i, g in reversed(gradient):
-            self.layers[i][0] -= g * self.lr
-
+        self._backprop(labels)
         return self.loss.forward(y, labels)
 
     def __str__(self):
@@ -64,16 +56,17 @@ class FeedForwardNet:
 
 np.random.seed(42)
 layers = [
-    DenseLayer(2, 3, Activations.ReLu()),
-    DenseLayer(3, 1, Activations.ReLu())
+    DenseLayer(2, 4, Activations.Sigmoid()),
+    DenseLayer(4, 1, Activations.Sigmoid()),
 ]
 
-myNet = FeedForwardNet(layers, Losses.MSE(), 0.001)
+myNet = FeedForwardNet(layers, Losses.MSE(), 0.004)
 print("Network:", myNet)
 
-inData = np.array([[1, 1]])
-labels = np.array([[4]])
-for i in range(5):
+inData = np.array([[2, 1], [1, 2]])
+labels = np.array([[1], [0]])
+
+for i in range(100000):
     out = myNet.forwardPass(inData)
     loss = myNet.fit(inData, labels)
     print(loss)