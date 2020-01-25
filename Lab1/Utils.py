e0207e19a3bd1261e034c080b1af0cd939be1d59
Acander <adrianz@kth.se>
2020-01-25 14:02:23 +0100

    Finished plot function

--- Lab1/Utils.py
+++ Lab1/Utils.py
@@ -2,14 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plotPoints(points):
+def plotPoints(points, line=None):
     '''
     Expects points to be a list of tuples:
         Tuple1: (xPoints, yPoints, color)
     '''
+    plt.xlabel('x', color='green')
+    plt.ylabel('y', color='green')
+
     for x, y, color in points:
         plt.plot(x, y, color)
+
+    xMin = np.min(np.concatenate(([p[0] for p in points]))) - 1
+    xMax = np.max(np.concatenate(([p[0] for p in points]))) + 1
+
+    yMin = np.min(np.concatenate(([p[1] for p in points]))) - 1
+    yMax = np.max(np.concatenate(([p[1] for p in points]))) + 1
+
+    if (line != None):
+        s, b = line
+        plotLinearDecisionBoundry(s, b, xMin-10, xMax+10)
+
+    setPlotDim([xMin, xMax], [yMin, yMax])
     plt.show()
+
+
+def setPlotDim(xAxis, yAxis):
+    plt.xlim(xAxis[0], xAxis[1])
+    plt.ylim(yAxis[0], yAxis[1])
 
 
 def generate2DNormal(numberOfPoints, origo, std):
@@ -33,3 +53,9 @@ def stackAndShuffleData(inData, labels):
 
 def plotLearningCurves(metrics):
     pass
+
+
+def plotLinearDecisionBoundry(slope, bias, min, max):
+    x = np.linspace(min, max, 100)
+    y = slope * x + bias
+    plt.plot(x, y, '-r')
