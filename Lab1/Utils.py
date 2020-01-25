773a7a107f4d08d181167fadf9692a91fe76f584
Acander <adrianz@kth.se>
2020-01-25 17:08:41 +0100

    Added 3D scatter plot to Utils

--- Lab1/Utils.py
+++ Lab1/Utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
 
 
 def plotPoints(points, line=None):
@@ -7,8 +9,9 @@ def plotPoints(points, line=None):
     Expects points to be a list of tuples:
         Tuple1: (xPoints, yPoints, color)
     '''
-    plt.xlabel('x', color='green')
-    plt.ylabel('y', color='green')
+    plt.xlabel('x1', color='green')
+    plt.ylabel('x2', color='green')
+    print(points)
 
     for x, y, color in points:
         plt.plot(x, y, color)
@@ -19,17 +22,40 @@ def plotPoints(points, line=None):
     yMin = np.min(np.concatenate(([p[1] for p in points]))) - 1
     yMax = np.max(np.concatenate(([p[1] for p in points]))) + 1
 
-    if (line != None):
+    if line != None:
         s, b = line
-        plotLinearDecisionBoundry(s, b, xMin-10, xMax+10)
+        plotLinearDecisionBoundry(s, b, xMin - 10, xMax + 10)
 
     setPlotDim([xMin, xMax], [yMin, yMax])
     plt.show()
 
 
-def setPlotDim(xAxis, yAxis):
+def setPlotDim(xAxis, yAxis, zAxis=None):
     plt.xlim(xAxis[0], xAxis[1])
     plt.ylim(yAxis[0], yAxis[1])
+
+def plot3D(points):
+    print(points)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    for x, y, z, color in points:
+        ax.scatter(x, y, z, color=color)
+
+    xMin = np.min(np.concatenate(([p[0] for p in points]))) - 1
+    xMax = np.max(np.concatenate(([p[0] for p in points]))) + 1
+
+    yMin = np.min(np.concatenate(([p[1] for p in points]))) - 1
+    yMax = np.max(np.concatenate(([p[1] for p in points]))) + 1
+
+    zMin = np.min(np.concatenate(([p[2] for p in points]))) - 1
+    zMax = np.max(np.concatenate(([p[2] for p in points]))) + 1
+
+    ax.set_xlim(xMin, xMax)
+    ax.set_ylim(yMin, yMax)
+    ax.set_zlim(zMin, zMax)
+
+    plt.show()
 
 
 def generate2DNormal(numberOfPoints, origo, std):
@@ -58,7 +84,6 @@ def generateData(pointsPerClass, centroids, colors, deviations):
         points.append((x, y, color))
 
     return points
-
 
 def plotLearningCurves(metrics):
     pass
