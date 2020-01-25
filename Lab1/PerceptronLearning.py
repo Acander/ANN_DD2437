dfea4c8cebc6e1b8e2e734fc75d07e33b5adca4e
Joey Öhman <joeyoh@kth.se>
2020-01-25 14:17:22 +0100

    Veri nice

--- Lab1/PerceptronLearning.py
+++ Lab1/PerceptronLearning.py
@@ -1,8 +1,7 @@
 import numpy as np
-from Lab1.LinearSeparableData import generateData
 
-# from Lab1.Utils import generate2DNormal, plotPoints
-from Lab1.Utils import plotPoints
+from Lab1.LinearSeparableData import getDecisionBoundry
+from Lab1.Utils import plotPoints, generateData, stackAndShuffleData
 
 
 class PLNet:
@@ -11,9 +10,12 @@ class PLNet:
         self.lr = learningRate
         self.weights = np.random.normal(0, 0.5, sizeIn + 1)
 
+    def predictRaw(self, x):
+        return np.matmul(self.weights, x)
+
     def predict(self, x):
         # self.weights @ np.vstack([x, ([1] * np.size(x[0]))])
-        return ((self.weights @ x > 0).astype(int) * 2) - 1  # convert to -1/1
+        return ((np.matmul(self.weights, x) > 0).astype(int) * 2) - 1  # convert to -1/1
 
     # Assumes labels are -1 or 1
     def fit(self, x, labels):
@@ -31,13 +33,51 @@ class PLNet:
             self.weights += self.lr * falsePrediction * (prediction * -1) * np.transpose(xCol)
 
 
-if __name__ == "__main__":
-    p1, p2 = generateData(10, ((2, 3), (5, 5)), ('ro', 'bo'), (1, 1))
+def getTrainedModel(x, y):
+    perceptron = PLNet(0.001, 2)
+    print(perceptron.weights)
+
+    N_EPOCHS = 50
+    for i in range(N_EPOCHS):
+        if i % 5 == 0:
+            print("Accuracy:", getAccuracy(perceptron, x, y))
+        perceptron.fit(x, y)
+
+    return perceptron
+
+
+def getAccuracy(model, x, y):
+    predictions = model.predict(x)
+    numCorrect = np.sum(np.equal(predictions, y).astype(int))
+    freqCorr = numCorrect / (N_CLASS * 2)
+    return freqCorr
+
+
+def generateStructureShuffleData(n_class, centroids, stdDevs):
+    p1, p2 = generateData(n_class, centroids, ('ro', 'bo'), stdDevs)
 
     x1 = np.vstack([p1[0], p1[1]])
     x2 = np.vstack([p2[0], p2[1]])
 
     labels1 = np.ones((1, len(p1[0])))
-    labels2 = np.zeros((1, len(p2[0])))
+    labels2 = np.zeros((1, len(p2[0]))) - 1
 
-    plotPoints(p1)
+    x, y = stackAndShuffleData([x1, x2], [labels1, labels2])
+    x = np.vstack([x, np.ones(N_CLASS * 2)])
+    return p1, p2, x, y[0]
+
+
+if __name__ == "__main__":
+    N_CLASS = 100
+    p1, p2, x, y = generateStructureShuffleData(N_CLASS, ((1, 3), (7, 9)), (1, 1))
+
+    net = getTrainedModel(x, y)
+    getAccuracy(net, x, y)
+
+    print(net.weights)
+    w1, w2, b = net.weights
+    print(getDecisionBoundry(w1, w2, b))
+    plotPoints([p1, p2])
+    xTest = np.array([[0, 0, 1], [8, 8, 1]]).T
+    print(net.predictRaw(xTest))
+    print(xTest)
