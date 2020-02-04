9fb4f30b57c20ccf0b1fad6801e70740fece8348
FreddeFrallan <freddefc@gmail.com>
2020-02-04 15:01:15 +0100

    Batch + multiple output backprop

--- Labb2/RBFNet.py
+++ Labb2/RBFNet.py
@@ -5,14 +5,14 @@ from Labb2.RBFFunc import RBF
 
 class RadialBasisFunctionNetwork:
 
-    def __init__(self, inputSize, outSize, hiddenSize, hiddenCentroids, RBF, lr=0.001, l1Dist=False):
+    def __init__(self, inputSize, outSize, hiddenSize, hiddenCentroids, RBF, lr=0.0001, l1Dist=False):
         self.outWeights = np.random.normal(0, 0.5, (hiddenSize, outSize))
         self.centroids = hiddenCentroids
         self.numHidden = hiddenSize
         self.l1Dist = l1Dist
         self.RBF = np.vectorize(RBF)
         self.previousHiddenValues = "CACHED FROM PREDICTION"
-        self.lr = np.array([[lr]])
+        self.lr = np.array([lr])
         assert hiddenCentroids.shape[0] == hiddenSize and hiddenCentroids.shape[1] == inputSize
 
     def predictDist(self, inputs):
@@ -35,33 +35,43 @@ class RadialBasisFunctionNetwork:
 
     def fit(self, inputs, labels):
         pred = self.predict(inputs)
-        # print("Pred\n", pred)
-        # print((pred - labels)**2)
-        loss = (pred - labels) ** 2  # Allows for batch
-        # print("Loss\n", loss, loss.shape)
-        # loss = loss.reshape(loss.shape + (1,))
-        #print(loss, loss.shape)
-        self.previousHiddenValues = self.previousHiddenValues
+        for i in range(pred.shape[1]):
+            # print(pred.shape, labels.shape)
+            # print("Pred\n", pred)
+            # print((pred - labels)**2)
+            loss = (pred - labels) ** 2  # Allows for batch
+            # print("Loss\n", loss, loss.shape)
+            # loss = loss.reshape(loss.shape + (1,))
+            # print(loss, loss.shape)
+            self.previousHiddenValues = self.previousHiddenValues
 
-        #print("Previous:", self.previousHiddenValues, self.previousHiddenValues.shape)
-        # self.previousHiddenValues = self.previousHiddenValues.reshape(1, self.previousHiddenValues.shape[0])
-        weightGrad = loss * self.previousHiddenValues
-        weightGrad = np.mean(weightGrad, axis=0)  # Batch Grad mean
-        weightGrad = weightGrad.reshape(weightGrad.shape + (1,))  # Reshape for broadcast
-        #print("Grad:", weightGrad, weightGrad.shape)
-        #print("Weights:", self.outWeights, self.outWeights.shape)
-        #print(self.lr, self.lr.shape)
-        self.outWeights += self.lr * weightGrad
+            # print("Previous:", self.previousHiddenValues, self.previousHiddenValues.shape)
+            # self.previousHiddenValues = self.previousHiddenValues.reshape(1, self.previousHiddenValues.shape[0])
+            weightGrad = loss[:, i].reshape((loss.shape[0], 1)) * self.previousHiddenValues
+            # print("Grad:", weightGrad, weightGrad.shape)
+            weightGrad = np.mean(weightGrad, axis=0)  # Batch Grad mean
+            weightGrad = weightGrad.reshape(weightGrad.shape + (1,))  # Reshape for broadcast
+            # print("Weights:", self.outWeights, self.outWeights.shape)
+            # print("Grad:", weightGrad, weightGrad.shape)
+            # print(self.lr, self.lr.shape)
+            # print((self.lr * weightGrad).shape)
+            self.outWeights[:, i] += self.lr * weightGrad[:, 0]
 
+            # input()
         return np.mean(loss)
 
 
 if __name__ == '__main__':
-    model = RadialBasisFunctionNetwork(2, 1, 4, np.array([[0, 0], [3, 3], [1, 1], [2, 2]]), RBF, l1Dist=False)
+    early = np.array([[0, 8], [3, 3], [1, 1], [2, 2]])
+    centroids = np.random.uniform(-1, 7, (4, 2))
+    print(centroids, centroids.shape)
+    print(early.shape)
+    model = RadialBasisFunctionNetwork(2, 2, 4, centroids, RBF, l1Dist=False)
     # print(model.predict(np.array([[2, 2], [3, 7]])))
-    inData = np.array([[2, 2]])
-    labels = np.array([[1]])
+    # [(points), () ]
+    inData = np.array([[2, 2], [0, 0], [3, 3]])
+    labels = np.array([[1, 3], [-1, 1], [5, 5]])
     for i in range(100000):
         loss = model.fit(inData, labels)
         print(loss)
-        # print("Predict", model.predict(inData))
+        #print("Predict", model.predict(inData))
