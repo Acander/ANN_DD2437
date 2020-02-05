1219cd7500eea73a9500a416d0a686aecce9f6ee
Joey Öhman <joeyoh@kth.se>
2020-02-05 19:35:48 +0100

    SOMSOMSOM

--- Labb2/SOM.py
+++ Labb2/SOM.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 from Labb2.CL import closestCentroid
+from Labb2.DataSetHandler import importDataSeta
 
 STEP_SIZE = 0.2
 SIGMA0 = 1
-TAU = 1
+TAU = 100
 
 
 def distToWinner(winner, nodes):
@@ -15,7 +16,15 @@ def distToWinner(winner, nodes):
 
 
 def neighbFunc(sigma, dist):
+    # print(sigma)
     return np.exp(- (dist * dist) / (2 * sigma * sigma))
+
+
+def meanLength(W):
+    # distSum = 0
+    # for i in range(len(W)):
+    #     distSum += np.linalg.norm(W[i])
+    return np.mean(np.linalg.norm(W, axis=1))
 
 
 def SOM1d(X, inSize, outSize, epochs=20):
@@ -34,14 +43,18 @@ def SOM1d(X, inSize, outSize, epochs=20):
                 # print(np.shape(W[wIdx]))
                 # print(np.shape(X[i] - W[wIdx]))
                 # print(np.linalg.norm(X[i] - W[wIdx]))
-                W[wIdx] += STEP_SIZE * h[wIdx] * np.linalg.norm(X[i] - W[wIdx])
+                W[wIdx] += STEP_SIZE * h[wIdx] * (X[i] - W[wIdx])
 
+    print(meanLength(W))
     winners = []
     for i in range(numCategories):
-        winners.append(closestCentroid(X[i], W))
-    print(np.sort(winners))
+        winners.append((i, closestCentroid(X[i], W)))
+    return sorted(winners, key=lambda x: x[1])
 
 
 if __name__ == '__main__':
-    X = 1337
-    SOM1d(np.random.normal(1, 1, (32, 84)), 84, 100)
+    X, animalNames = importDataSeta()
+    # SOM1d(np.random.normal(1, 1, (32, 84)), 84, 100)
+    similaritySequence = SOM1d(X, 84, 100, epochs=20)
+    animalsOrdered = [animalNames[animal[0]] for animal in similaritySequence]
+    print(animalsOrdered)
