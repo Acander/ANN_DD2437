597983f87cf30eef76fe98bbd4e3604a2be86f3f
Acander <adrianz@kth.se>
2020-02-05 19:56:21 +0100

    fuck

--- Labb2/DataSetHandler.py
+++ Labb2/DataSetHandler.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def importAnimalDataSet():
     namesString = open("data_lab2/animalnames.txt", 'r').read()
     names = np.array(list(namesString.split("\n")))
@@ -9,20 +10,6 @@ def importAnimalDataSet():
     attributeArray = attributeArray.reshape((32, 84))
     return attributeArray, [animal.strip("\t\'") for animal in names]
 
-def importBallisticDataSet():
-    attributesString = open("data_lab2/ballist.dat", 'r').read()
-    trainingPairStringList = attributesString.split("\n")
-    trainingDataset = []
-    for i in range(len(trainingPairStringList)-1):
-        trainingPair = trainingPairStringList[i].split("\t")
-        #print(trainingPair)
-        input = tuple(map(float, trainingPair[0].split(" ")))
-        output = tuple(map(float, trainingPair[1].split(" ")))
-
-        trainingDataset.append((input, output))
-        #print(input, output)
-
-    return np.array(trainingDataset)
 
 def importTrainingBallisticData():
     attributesString = open("data_lab2/ballist.dat", 'r').read()
@@ -39,23 +26,32 @@ def importTrainingBallisticData():
 
     return np.array(trainingDataset)
 
+
 def importTestBallisticData():
     attributesString = open("data_lab2/balltest.dat", 'r').read()
-    trainingPairStringList = attributesString.split("\n")
-    trainingDataset = []
-    for i in range(len(trainingPairStringList) - 1):
-        trainingPair = trainingPairStringList[i].split("\t")
+    testPairStringList = attributesString.split("\n")
+    testDataset = []
+    for i in range(len(testPairStringList) - 1):
+        trainingPair = testPairStringList[i].split("\t")
         # print(trainingPair)
         input = tuple(map(float, trainingPair[0].split(" ")))
 
-        trainingDataset.append(input)
+        testDataset.append(input)
         # print(input, output)
 
-    return np.array(trainingDataset)
+    return np.array(testDataset)
 
-trainingDataSet = importBallisticDataSet()
-a, b = importAnimalDataSet()
-print(b)
 
-#print(trainingDataSet)
-#print(trainingDataSet[0][0][0])
+if __name__ == '__main__':
+    print("TRAIN")
+    print(importTrainingBallisticData()[0][4][1])
+    print("TEST")
+    print(importTestBallisticData()[10][])
+
+    print(imp)
+
+    # a, b = importAnimalDataSet()
+    # print(b)
+
+    # print(trainingDataSet)
+    # print(trainingDataSet[0][0][0])
