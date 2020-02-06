921baca35d23dbad2d67b349672ffdf83c83c912
Joey Öhman <joeyoh@kth.se>
2020-02-06 07:20:37 +0100

    cyclic tour....

--- Labb2/SOM.py
+++ Labb2/SOM.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from Labb2.CL import closestCentroid
+from Labb2.DataHandler import plotPointsXY
 from Labb2.DataSetHandler import importAnimalDataSet
 
 STEP_SIZE = 0.2
@@ -18,24 +19,34 @@ def distToWinnerChain(winner, nodes):
 def distToWinnerCircular(winner, nodes):
     n = len(nodes)
     indices = np.arange(n)
-    return np.min(
-        np.abs(winner - indices),
-        n - indices + 1 + winner)
+    # print(winner)
+    # print(nodes)
+    vec1 = np.abs(winner - indices)
+    vec2 = np.array(n - indices + 1 + winner)
+    '''
+    print("vec1:", vec1)
+    print("vec2:", vec2)
+    print(len(vec1))
+    print(len(vec2))
+    print(vec1[0])
+    print(vec2[0])
+    print(min(vec1[0], vec2[0]))
+    '''
+    return np.array([min(vec1[i], vec2[i]) for i in range(len(vec1))])
+    # return np.min(
+    #     np.abs(winner - indices),
+    #     n - indices + 1 + winner)
 
 
 def neighbFunc(sigma, dist):
-    # print(sigma)
     return np.exp(- (dist * dist) / (2 * sigma * sigma))
 
 
 def meanLength(W):
-    # distSum = 0
-    # for i in range(len(W)):
-    #     distSum += np.linalg.norm(W[i])
     return np.mean(np.linalg.norm(W, axis=1))
 
 
-def SOM(X, inSize, outSize, epochs=20, distFunc=distToWinnerChain):
+def SOM(X, inSize, outSize, epochs=20, distFunc=distToWinnerChain, sort=True):
     numCategories = len(X)
     W = np.random.uniform(0, 1, (outSize, inSize))  # These are the nodes, each row defines a centroid
 
@@ -57,7 +68,7 @@ def SOM(X, inSize, outSize, epochs=20, distFunc=distToWinnerChain):
     winners = []
     for i in range(numCategories):
         winners.append((i, closestCentroid(X[i], W)))
-    return sorted(winners, key=lambda x: x[1])
+    return sorted(winners, key=lambda x: x[1]) if sort else winners
 
 
 def somAnimals():
@@ -68,10 +79,17 @@ def somAnimals():
 
 
 def somCyclicTour():
-    X = np.array([np.array([i, i ** 2]) for i in range(10)])
+    X = np.random.uniform(0.0, 1.0, (10, 2))
     print(X)
-    similaritySequence = SOM(X, 2, 10, epochs=20, distFunc=distToWinnerCircular)
+    similaritySequence = SOM(X, 2, 10, epochs=20, distFunc=distToWinnerCircular, sort=False)
     print(similaritySequence)
+    xPlot = [X[i][0] for i, winner in similaritySequence]
+    yPlot = [X[i][1] for i, winner in similaritySequence]
+    xPlot.append(X[0][0])
+    yPlot.append(X[1][0])
+    # end not included? no cycle in plot
+    print([(xPlot, yPlot)])
+    plotPointsXY([(xPlot, yPlot)], ["lol"], True)
 
 
 if __name__ == '__main__':
