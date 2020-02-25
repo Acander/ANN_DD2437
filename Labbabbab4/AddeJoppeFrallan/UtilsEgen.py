e817166ece693a049cf962ad3ff900c284eca02d
Joey Öhman <joeyoh@kth.se>
2020-02-25 16:41:48 +0100

    Plots

--- Labbabbab4/AddeJoppeFrallan/UtilsEgen.py
+++ Labbabbab4/AddeJoppeFrallan/UtilsEgen.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
+hToPlotIndices = None
 
 
 def meanReconstLossOnParts(model, dataParts):
     sizes = [len(d) for d in dataParts]
     losses = [meanReconstLossTestSet(model, p).numpy() for p in dataParts]
-    return np.sum(losses * (sizes / np.sum(sizes))) # Weighted average, by the length in each part
+    return np.sum(losses * (sizes / np.sum(sizes)))  # Weighted average, by the length in each part
 
 
 def meanReconstLossTestSet(rbm, testSet):
@@ -16,6 +19,35 @@ def meanReconstLossTestSet(rbm, testSet):
 
 def meanReconstLoss(v0, v1):
     return tf.reduce_mean(tf.reduce_sum(tf.abs(v1 - v0), axis=1)) / v1.shape[1]
+
+
+def plotLearningCurves(lossesList, labels):
+    X = np.arange(len(lossesList))
+    for i in range(lossesList):
+        plt.plot(X, lossesList[i], labels[i])
+
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Reconstruction Loss")
+    plt.show()
+
+
+def plotWeights(weights, epochId):
+    image_size = (28, 28)
+    hLen = weights.shape[1]
+    numToPlot = 10
+
+    global hToPlotIndices
+    if hToPlotIndices is None:
+        hToPlotIndices = np.random.choice(np.arange(hLen), numToPlot, replace=False).tolist()
+        # hToPlotIndices = [int(h) for h in hToPlotIndices]
+
+    # if epoch % self.rf["period"] == 0 and self.is_bottom:
+    from Labbabbab4.codeAlaPawel.util import viz_rf
+    print(hToPlotIndices)
+    wToPlot = np.array([weights[:, h] for h in hToPlotIndices])
+    viz_rf(weights=wToPlot.reshape((image_size[0], image_size[1], -1)),
+           it=epochId, grid=[2, int(numToPlot/2)])
 
 
 def energyAvg(visible, hidden, weights, biasV, biasH, matrixOps=True):
